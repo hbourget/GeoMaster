d8d9ed7dd9e5fa1a961f97463be3884143fe32ef
ozaki <29860391+OzakIOne@users.noreply.github.com>
2023-10-03 14:04:53 +0200

    fix: #1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const emotionCache = createCache({ key: 'emotion-css-cache', prepend: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CacheProvider value={emotionCache}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </CacheProvider>
   </React.StrictMode>,
 );
