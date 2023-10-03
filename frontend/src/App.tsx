8b4a3bdd458e2333d41cbdd3a44347cac4b9169b
ozaki <29860391+OzakIOne@users.noreply.github.com>
2023-10-03 11:37:38 +0200

    chore: prettier, docker, compose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { css } from '../styled-system/css'
-import { Button } from '@chakra-ui/react'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { css } from '../styled-system/css';
+import { Button } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,7 +28,7 @@ function App() {
       </div>
       <p>Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
