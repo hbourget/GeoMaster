a6f101d9a443eced5d701ae0fea17aa3e71d5498
ozaki <29860391+OzakIOne@users.noreply.github.com>
2024-01-11 15:20:12 +0100

    panda 26 test

--- frontend/panda.config.ts
+++ frontend/panda.config.ts
@@ -7,16 +7,29 @@ export default defineConfig({
   // Where to look for your css declarations
   include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 
-  strictTokens: false,
+  strictTokens: true,
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        zIndex: {
+          first: {
+            value: 1,
+          },
+        },
+        borderWidths: {
+          1: {
+            value: '1px',
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
-  outdir: './styled-system',
+  outdir: 'styled-system',
 });
