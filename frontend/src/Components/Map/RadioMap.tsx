e5749ec18ea06549e7f21fa8edbf965622d281aa
ozaki <29860391+OzakIOne@users.noreply.github.com>
2024-01-12 17:20:45 +0100

    timer game logic

--- frontend/src/Components/Map/RadioMap.tsx
+++ frontend/src/Components/Map/RadioMap.tsx
@@ -21,10 +21,17 @@ const infoRoomStyle = css({
 });
 
 const RadioMap = () => {
+  const GAME_TIMER = 2;
+  const GAME_ITERATION = 2;
+  const GAME_TYPE = 3;
+
   const [selectedLocation, setSelectedLocation] = useState(null);
-  const [timer, setTimer] = useState(10);
-  console.log('timer:', timer);
+  const [timer, setTimer] = useState(GAME_TIMER);
   const [currentGameState, setCurrentGameState] = useAtom(currentGamePlaying);
+  const [arrayData, setArrayData] = useState([]);
+  const [iteration, setIteration] = useState(GAME_ITERATION);
+  const [gameType, setGameType] = useState(GAME_TYPE); // 3 country, 2 city, 1 flag
+  const [gameEnd, setGameEnd] = useState(false);
 
   const handleLocationMouseOver = (event) => {
     const LocationName = getLocationName(event);
@@ -34,6 +41,11 @@ const RadioMap = () => {
   };
 
   useEffect(() => {
+    if (gameEnd) {
+      console.log('GAME END');
+      return;
+    }
+
     const interval = setInterval(() => {
       if (timer > 0) {
         setTimer(timer - 1);
@@ -41,13 +53,35 @@ const RadioMap = () => {
         // todo 1 send a request to the server with the selectedLocation
         // todo 2 update the country to guess
         // todo 3 reset the timer to 10
+        arrayData.push(selectedLocation);
+        setIteration(iteration - 1);
+        setTimer(GAME_TIMER);
+        // TODO reset user selection ?
+        console.log('timer ended');
+        if (iteration === 1) {
+          console.log('arrayData:', arrayData);
+          setCurrentGameState('end');
+          setGameType(gameType - 1);
+          // TODO make a request to the server
+          setIteration(GAME_ITERATION);
+          setArrayData([]);
+          if (gameType === 1) {
+            console.log('END OF THE GAME');
+
+            setGameEnd(true);
+          }
+        }
       }
     }, 1000);
-    return () => clearInterval(interval);
-  }, [timer]);
+    return () => clearInterval(interval); // Clear the interval when the component unmounts
+  }, []);
 
   const handleOnChange = (selectedNode) => {
-    setSelectedLocation(selectedNode.attributes.name.value);
+    const country = selectedNode.attributes.name.value;
+    console.log('country:', country);
+    // if (!selectedLocation === country) {
+    setSelectedLocation(country);
+    // }
   };
 
   return (
