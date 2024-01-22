98e1f008d100ca6e1e443862e77786ffa35a4c89
ozakione <29860391+OzakIOne@users.noreply.github.com>
2024-01-22 15:16:32 +0100

    zoom

--- frontend/src/Components/Map/RadioMap.tsx
+++ frontend/src/Components/Map/RadioMap.tsx
@@ -7,6 +7,7 @@ import { useAtom } from 'jotai';
 import { currentGameID, currentUserID, flagGuess, mapGuess, monumentGuess } from '../../jotai';
 import { useMutation } from '@tanstack/react-query';
 import { Text } from '@chakra-ui/react';
+import { useRef } from 'react';
 
 const sendGameData = async (userId: number, gameId: number, gameData: string[]) => {
   const filteredArray = gameData.map((value) => (value === null ? 'Unknown' : value));
@@ -31,6 +32,27 @@ const sendGameData = async (userId: number, gameId: number, gameData: string[]) 
   }
 };
 
+const containerStyle = css({
+  width: '100%',
+  marginTop: '2%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: 'white',
+  overflow: 'hidden',
+});
+
+const zoomControlStyle = css({
+  display: 'flex',
+  justifyContent: 'center',
+  marginBottom: '10px',
+  width: '100%',
+  position: 'absolute',
+  top: '10px',
+  zIndex: '2',
+});
+
 const RadioMap = () => {
   const GAME_TIMER = 10;
   const GAME_ITERATION = 5;
@@ -52,6 +74,15 @@ const RadioMap = () => {
   const guessIteration = 5 - iteration;
   const [gameType, setGameType] = useState(GAME_TYPE);
   const [gameEnd, setGameEnd] = useState(false);
+
+  // MAX
+  const [zoomLevel, setZoomLevel] = useState(1);
+  const [panning, setPanning] = useState(false);
+  const [panStart, setPanStart] = useState({ x: 0, y: 0 });
+  const [panOffset, setPanOffset] = useState({ x: 0, y: 0 });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const locationsRef = useRef<SVGGElement | null>(null);
+  // MAX
 
   const handleLocationMouseOver = (event) => {
     getLocationName(event);
@@ -121,6 +152,48 @@ const RadioMap = () => {
     // }
   };
 
+  // MAX
+  const handleZoom = (deltaY) => {
+    setZoomLevel((prevZoom) => {
+      const newZoom = deltaY > 0 ? prevZoom - 0.1 : prevZoom + 0.1;
+      return Math.max(1, Math.min(newZoom, 3));
+    });
+  };
+
+  const handlePanStart = (event) => {
+    setPanning(true);
+    setPanStart({ x: event.clientX, y: event.clientY });
+  };
+
+  const handlePanMove = (event) => {
+    if (panning) {
+      const dx = event.clientX - panStart.x;
+      const dy = event.clientY - panStart.y;
+
+      setPanOffset({ x: panOffset.x + dx, y: panOffset.y + dy });
+      setPanStart({ x: event.clientX, y: event.clientY });
+    }
+
+    setMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const handlePanEnd = () => {
+    setPanning(false);
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    handleZoom(event.deltaY);
+  };
+
+  useEffect(() => {
+    if (locationsRef.current) {
+      setZoomLevel(1);
+      setPanOffset({ x: 0, y: 0 });
+    }
+  }, []);
+  // MAX
+
   return (
     <div
       style={{ marginTop: '1%' }}
@@ -137,9 +210,9 @@ const RadioMap = () => {
             You have to guess{' '}
             {gameType === 3 ? 'the country' : gameType === 2 ? 'the city' : 'the flag'} of{' '}
             {gameType === 3 ? (
-              <Text fontSize="2xl" color="white">
-                {countriesFlag[guessIteration]}
-              </Text>
+              <img
+                src={`https://restfulcountries.com//assets//images//flags//${countriesFlag[guessIteration]}.png`}
+              ></img>
             ) : gameType === 2 ? (
               <Text fontSize="2xl" color="white">
                 {countriesMap[guessIteration]}
@@ -153,11 +226,42 @@ const RadioMap = () => {
           <p style={{ color: 'white' }}>Temps restant : {timer}</p>
         </>
       )}
-      <RadioSVGMap
-        map={World}
-        onLocationMouseOver={handleLocationMouseOver}
-        onChange={handleOnChange}
-      />
+
+      <div
+        className={containerStyle}
+        onMouseMove={handlePanMove}
+        onMouseUp={handlePanEnd}
+        onWheel={handleWheel}
+      >
+        <div
+          className={zoomControlStyle}
+          style={{
+            transformOrigin: `${
+              ((mousePosition.x - panOffset.x) / locationsRef.current?.clientWidth) * 100
+            }% ${((mousePosition.y - panOffset.y) / locationsRef.current?.clientHeight) * 100}%`,
+          }}
+        ></div>
+
+        <div
+          style={{
+            width: '60%',
+            height: '100%',
+            margin: 'auto',
+            overflow: 'hidden',
+            transform: `scale(${zoomLevel}) translate(${panOffset.x}px, ${panOffset.y}px)`,
+            cursor: panning ? 'grabbing' : 'grab',
+          }}
+          onMouseDown={handlePanStart}
+          ref={locationsRef}
+        >
+          <RadioSVGMap
+            map={World}
+            onLocationMouseOver={handleLocationMouseOver}
+            // onLocationMouseOut={handleLocationMouseOut}
+            onChange={handleOnChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
