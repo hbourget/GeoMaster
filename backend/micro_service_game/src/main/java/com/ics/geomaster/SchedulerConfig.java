949e48ab0a9c2cd1388593a8045190ba2a71affb
Hugo <hugobourget@protonmail.com>
2024-01-24 17:16:25 +0100

    feat: scheduled task remove old games

---
+++ backend/micro_service_game/src/main/java/com/ics/geomaster/SchedulerConfig.java
@@ -0,0 +1,37 @@
+package com.ics.geomaster;
+
+import com.ics.geomaster.game.controllers.GameService;
+import com.ics.geomaster.game.models.Game;
+import com.ics.geomaster.game.models.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+    @Autowired
+    private GameService cService;
+    private GameRepository gameRepository;
+    @Transactional
+    @Scheduled(cron = "0 * * * * *")
+    public void removeOldGames() {
+
+        Iterable<Game> games = gameRepository.findAll();
+
+        for (Game game : games) {
+            LocalDateTime gameCreationDate = LocalDateTime.parse(game.getCreationDate(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            LocalDateTime now = LocalDateTime.now();
+            if (gameCreationDate.isBefore(now.minusMinutes(10))) {
+                System.out.println("Game " + game.getId() + " is older than 10 minutes, deleting it...");
+                cService.deleteGame(game.getId());
+            }
+        }
+
+    }
+}
