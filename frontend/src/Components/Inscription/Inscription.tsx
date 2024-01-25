7ed207a569884b1a6336f40d727cd273af39d876
maxxben <max.benucci@orange.fr>
2024-01-25 23:48:29 +0100

    fix tipo

--- frontend/src/Components/Inscription/Inscription.tsx
+++ frontend/src/Components/Inscription/Inscription.tsx
@@ -11,7 +11,7 @@ const formStyle = css({
   margin: 'auto',
   padding: '20px',
   borderRadius: '8px',
-  boxShadow: '0 0 10px ', //#004590
+  boxShadow: '0 0 10px black', //#004590
   marginBottom: '2%',
   color: 'white',
   textAlign: 'center',
